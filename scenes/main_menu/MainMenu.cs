0fc16eb2c70350de4a4e9b259918c35ef90d3e41
FrederikHR <frederik.rosenvinge@gmail.com>
2023-08-03 11:26:11 +0200

    introscreen finished

---
+++ scenes/main_menu/MainMenu.cs
@@ -0,0 +1,64 @@
+using Godot;
+using System;
+
+public partial class MainMenu : Control
+{
+	Texture2D StartSelected;
+	Texture2D StartUnselected;
+	Texture2D OptionsSelected;
+	Texture2D OptionsUnselected;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		var StartButtonTexture = GetNode<TextureRect>("Start/StartTexture");
+		StartSelected =  (Texture2D)ResourceLoader.Load("res://art/ui/startbutton_selected.png");
+		StartUnselected = (Texture2D)ResourceLoader.Load("res://art/ui/startbutton.png");
+		OptionsSelected = (Texture2D)ResourceLoader.Load("res://art/ui/optionsbutton_selected.png");
+		OptionsUnselected = (Texture2D)ResourceLoader.Load("res://art/ui/optionsbutton.png");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	private void _on_start_button_down()
+	{
+		var StartButtonTexture = GetNode<TextureRect>("Start/StartTexture");
+		StartButtonTexture.Texture = StartSelected;	
+	
+	}
+	
+	
+	private void _on_start_button_up()
+	{
+		var StartButtonTexture = GetNode<TextureRect>("Start/StartTexture");
+		StartButtonTexture.Texture = StartUnselected;	
+	}
+	
+	private void _on_options_button_down()
+	{
+		var OptionsButtonTexture = GetNode<TextureRect>("Options/OptionsTexture");
+		OptionsButtonTexture.Texture = OptionsSelected;
+	}
+	
+	private void _on_options_button_up()
+	{
+		var OptionsButtonTexture = GetNode<TextureRect>("Options/OptionsTexture");
+		OptionsButtonTexture.Texture = OptionsUnselected;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

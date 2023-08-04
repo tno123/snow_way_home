62ae7eabfd5b13a442e64b543e522256c6b8ecba
Tor-Arne <tornord94@gmail.com>
2023-08-04 05:18:24 +0200

    Added Ice Physics

--- scenes/snowball/Snowball.cs
+++ scenes/snowball/Snowball.cs
@@ -3,14 +3,24 @@ using System;
 
 public partial class Snowball : CharacterBody2D
 {
+	[Export]
+	public float IceSpeed = 10.0f;
+
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
+
+	public const float CoyoteTime = 0.1f;
+
+	public TileMap tileMap;
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
 	public override void _PhysicsProcess(double delta)
 	{
+		tileMap = GetParent().GetNode<TileMap>("TileMap");
+		bool isOnIce = false;
+
 		Vector2 velocity = Velocity;
 
 		// Add the gravity.
@@ -30,11 +40,25 @@ public partial class Snowball : CharacterBody2D
 		}
 		else
 		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			// Do try except on get last slide collision
+			try {
+				var colliderRid = GetLastSlideCollision().GetColliderRid();
+				if (colliderRid != null)
+				{
+					var tileData = tileMap.GetCellTileData(0,tileMap.GetCoordsForBodyRid(colliderRid));
+					if ((bool)tileData.GetCustomData("Ice") == true && IsOnFloor()) {
+						isOnIce = true;
+					}
+				}
+			} catch (NullReferenceException) {
+				// Do nothing
+			}
+			
+			//Friction
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, isOnIce? IceSpeed : Speed);
 		}
 
 		Velocity = velocity;
 		MoveAndSlide();
-
 	}
 }

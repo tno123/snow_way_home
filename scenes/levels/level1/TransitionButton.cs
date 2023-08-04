d61a59377a6848cc9f15a4b23a59c2728db84af9
FrederikHR <frederik.rosenvinge@gmail.com>
2023-08-04 15:29:32 +0200

    code that could be removed (commented out atm)

--- scenes/levels/level1/TransitionButton.cs
+++ scenes/levels/level1/TransitionButton.cs
@@ -1,29 +1,29 @@
-using Godot;
-using System;
-
-public partial class TransitionButton : CheckButton
-{
-	[Export]
-	public Transitions transitions;
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
-	/*
-	private void _on_toggled(bool button_pressed)
-	{
-		GD.Print(button_pressed);
-		transitions.SetNextAnimation(button_pressed);
-		
-	}
-	*/
-
-}
-
-
-
+//using Godot;
+//using System;
+//
+//public partial class TransitionButton : CheckButton
+//{
+//	[Export]
+//	public Transitions transitions;
+//	// Called when the node enters the scene tree for the first time.
+//	public override void _Ready()
+//	{
+//	}
+//
+//	// Called every frame. 'delta' is the elapsed time since the previous frame.
+//	public override void _Process(double delta)
+//	{
+//	}
+//	/*
+//	private void _on_toggled(bool button_pressed)
+//	{
+//		GD.Print(button_pressed);
+//		transitions.SetNextAnimation(button_pressed);
+//
+//	}
+//	*/
+//
+//}
+//
+//
+//
--- scenes/main_menu/Start.cs
+++ scenes/main_menu/Start.cs
@@ -1,26 +1,26 @@
-using Godot;
-using System;
-
-public partial class Start : Button
-{
-	[Export]
-	public Transitions transitions;
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
-	/*
-	private void _on_toggled(bool button_pressed)
-	{
-		transitions.SetNextAnimation(button_pressed);
-	}
-	*/
-}
-
-
-
+//using Godot;
+//using System;
+//
+//public partial class Start : Button
+//{
+//	[Export]
+//	public Transitions transitions;
+//	// Called when the node enters the scene tree for the first time.
+//	public override void _Ready()
+//	{
+//	}
+//
+//	// Called every frame. 'delta' is the elapsed time since the previous frame.
+//	public override void _Process(double delta)
+//	{
+//	}
+//	/*
+//	private void _on_toggled(bool button_pressed)
+//	{
+//		transitions.SetNextAnimation(button_pressed);
+//	}
+//	*/
+//}
+//
+//
+//

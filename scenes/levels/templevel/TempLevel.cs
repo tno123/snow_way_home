a0a3e91450e48468af3cfa7763480826840f5983
FrederikHR <frederik.rosenvinge@gmail.com>
2023-08-05 00:54:05 +0200

    respawn mechanic

--- scenes/levels/templevel/TempLevel.cs
+++ scenes/levels/templevel/TempLevel.cs
@@ -4,15 +4,42 @@ using System;
 
 public partial class TempLevel : Node
 {
+	[Export]
+	public Transitions transition;
 	
+	Camera2D camera;
+	CharacterBody2D snowball;
+	private string currentScenePath;
+	
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		camera = GetNode<Camera2D>("Camera2D");
+		snowball = GetNode<CharacterBody2D>("Snowball");
+		transition.SetNextAnimation("fade_in");
+		currentScenePath = "res://scenes/levels/templevel/tempLevel.tscn";
+	
+		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+			//GetTree().reload_current_scene();
+			if (snowball.Position.Y > camera.LimitBottom){
+				GD.Print(snowball.Position.Y);
+				GD.Print(camera.LimitBottom);
+				var sceneManager = GetNode<SceneManager>("/root/SceneManager");
+				//GetTree().ChangeScene(currentScenePath);
+				sceneManager.GotoScene(currentScenePath);
+
+
+			}
+			
+			
+			
+			
 	}
 }
 

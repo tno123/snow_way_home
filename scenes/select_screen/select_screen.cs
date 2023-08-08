039c3a3b692bf32cfb158d6e667ad1b252ea137a
FrederikHR <frederik.rosenvinge@gmail.com>
2023-08-08 09:28:08 +0200

    stuff

--- scenes/select_screen/select_screen.cs
+++ scenes/select_screen/select_screen.cs
@@ -4,6 +4,7 @@ using System;
 public partial class select_screen : Control
 {
 	private AnimationPlayer animationPlayer;
+	private string levelSelected;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -15,14 +16,36 @@ public partial class select_screen : Control
 	{
 	}
 	
-	private void _on_button_pressed(StringName buttonName)
+	private void _on_button_pressed(string buttonName, string level=null)
 	{
+	
 		GD.Print(buttonName);
+		GD.Print(level);
 		if (animationPlayer != null){
 			animationPlayer.Stop();
 		}
 		string path = buttonName.ToString() + "/blink";
 		animationPlayer = GetNode<AnimationPlayer>(path);
+		//levelSelected="res://scenes/levels/level1/Level_1.tscn";
+		levelSelected=level;
 		animationPlayer.Play("new_animation");
+
+	}
+	private void _on_start_pressed()
+	{
+	
+		if (levelSelected != null){
+			GD.Print(levelSelected != null);
+
+			var sceneManager = GetNode<SceneManager>("/root/SceneManager");
+			sceneManager.GotoScene(levelSelected);
+		}
+
 	}
 }
+
+
+
+
+
+

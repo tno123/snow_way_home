a964c432e9d834ed8680d766f74d719f490fbffc
FrederikHR <frederik.rosenvinge@gmail.com>
2023-08-10 10:23:04 +0200

    shine effect added

---
+++ scenes/effects/Shine.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public partial class Shine : Sprite2D
+{
+	private AnimatedSprite2D animation;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		animation = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		animation.Play("main");
+	}
+}

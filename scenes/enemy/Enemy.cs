e2e787305285452653cc4496419362c073608ecd
Tor-Arne <tornord94@gmail.com>
2023-08-19 16:43:11 +0200

    test

--- scenes/enemy/Enemy.cs
+++ scenes/enemy/Enemy.cs
@@ -3,14 +3,33 @@ using System;
 
 public partial class Enemy : CharacterBody2D
 {
+	[Export]
+	public Line2D LineOfSight;
+
+	[Export]
+	public float MinDistance = 200.0f;
+
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
+
+	public Snowball Snowball;
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
+	public override void _Ready()
+	{
+		//Add points to the LineOfSight.
+		LineOfSight.AddPoint(Position,0);
+		Snowball = GetParent().GetNode<Snowball>("Snowball");
+		LineOfSight.AddPoint(Snowball.Position,1);
+	}
+
 	public override void _PhysicsProcess(double delta)
 	{
+		UpdateLineOfSight();
+
+
 		Vector2 velocity = Velocity;
 
 		// Add the gravity.
@@ -39,5 +58,22 @@ public partial class Enemy : CharacterBody2D
 
 		Velocity = velocity;
 		MoveAndSlide();
+	}
+
+	private void UpdateLineOfSight()
+	{
+		LineOfSight.ClearPoints();
+		LineOfSight.AddPoint(Position);
+		LineOfSight.AddPoint(Snowball.Position);
+		// if length of line if shorter than 100, color line green, else color it red
+		if (LineOfSight.GetPointPosition(0).DistanceTo(LineOfSight.GetPointPosition(1)) < MinDistance)
+		{
+			LineOfSight.DefaultColor = new Color(0, 1, 0);
+		}
+		else
+		{
+			LineOfSight.DefaultColor = new Color(1, 0, 0);
+		}
+
 	}
 }

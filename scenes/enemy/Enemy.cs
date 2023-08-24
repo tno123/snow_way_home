9e5f586012e42807cb3600a26f2599a978cc272b
Tor-Arne <tornord94@gmail.com>
2023-08-24 04:17:49 +0200

    movement and animation of enemy

--- scenes/enemy/Enemy.cs
+++ scenes/enemy/Enemy.cs
@@ -9,13 +9,17 @@ public partial class Enemy : CharacterBody2D
 	[Export]
 	public float MinDistance = 200.0f;
 
-	public const float Speed = 300.0f;
+	public const float Speed = 100.0f;
 	public const float JumpVelocity = -400.0f;
 
 	public Snowball Snowball;
+	public AnimationPlayer AnimationPlayer;
+	public Sprite2D Sprite2D;
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+
+	private string mode = "idle";
 
 	public override void _Ready()
 	{
@@ -23,18 +27,41 @@ public partial class Enemy : CharacterBody2D
 		LineOfSight.AddPoint(Position,0);
 		Snowball = GetParent().GetNode<Snowball>("Snowball");
 		LineOfSight.AddPoint(Snowball.Position,1);
+
+		AnimationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+		Sprite2D = GetNode<Sprite2D>("Sprite2D");
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
 		UpdateLineOfSight();
 
+		AnimationPlayer.Play(mode);
 
 		Vector2 velocity = Velocity;
 
 		// Add the gravity.
 		if (!IsOnFloor())
 			velocity.Y += gravity * (float)delta;
+
+		//if mode is walk, walk towards snowball. Take care to flip the sprite if the snowball is to the left.
+		if (mode == "walk")
+		{
+			if (Position.X < Snowball.Position.X)
+			{
+				velocity.X = Speed;
+				Sprite2D.FlipH = false;
+			}
+			else
+			{
+				velocity.X = -Speed;
+				Sprite2D.FlipH = true;
+			}
+		}
+		else
+		{
+			velocity.X = 0;
+		}
 
 
 		/*
@@ -69,10 +96,12 @@ public partial class Enemy : CharacterBody2D
 		if (LineOfSight.GetPointPosition(0).DistanceTo(LineOfSight.GetPointPosition(1)) < MinDistance)
 		{
 			LineOfSight.DefaultColor = new Color(0, 1, 0);
+			mode = "walk";
 		}
 		else
 		{
 			LineOfSight.DefaultColor = new Color(1, 0, 0);
+			mode = "idle";
 		}
 
 	}

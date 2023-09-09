f617b204d8ca6d1d6f894434a34ce695a9d4a7a0
FrederikHR <frederik.rosenvinge@gmail.com>
2023-09-09 23:58:00 +0200

    removed exclamation from script

--- scenes/levels/townLevel/TownLevel.cs
+++ scenes/levels/townLevel/TownLevel.cs
@@ -3,7 +3,7 @@ using System;
 
 public partial class TownLevel : Node
 {
-	AnimatedSprite2D Exclamation;
+	
 	// Called when the node enters the scene tree for the first time.
 	
 	public override void _Ready()

930d49d38809385c63042084dcc409d16b303318
Tor-Arne <tornord94@gmail.com>
2023-09-16 19:37:02 +0200

    basic avalanche + tweaked falling damage + more mountain level

--- scenes/levels/level3/MountainLevel.cs
+++ scenes/levels/level3/MountainLevel.cs
@@ -3,15 +3,23 @@ using System;
 
 public partial class MountainLevel : Node
 {
-	[Export]
-	public Polygon2D polygon;
-
-	[Export]
-	public CollisionPolygon2D collisionPolygon;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		polygon.Polygon = collisionPolygon.Polygon;
+		//Get all CollisionPolygon2D that are child nodes of Ground
+		var ground = GetNode<Node2D>("Ground");
+		var groundChildren = ground.GetChildren();
+
+		for (int i = 0; i < groundChildren.Count; i++)
+		{
+			if (groundChildren[i] is CollisionPolygon2D)
+			{
+				var colPolygon = (CollisionPolygon2D)groundChildren[i];
+				var polygon = colPolygon.GetNode<Polygon2D>("Polygon2D");
+				polygon.Polygon = colPolygon.Polygon;
+			}
+		}
+		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.

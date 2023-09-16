a565374e8eaf2fb82007204257498d900011651d
FrederikHR <frederik.rosenvinge@gmail.com>
2023-09-16 15:38:59 +0200

    villager mob init

--- scenes/VillagerMob.cs
+++ scenes/VillagerMob.cs
@@ -1,25 +1,25 @@
 using Godot;
 using System;
 
-public partial class Villager : CharacterBody2D
+public partial class VillagerMob : CharacterBody2D
 {
 	[Export]
 	public float MinDistance = 2000.0f;
 	[Export]
 	public Line2D LineOfSight;
 
-	public const float Speed = 150.0f;
+	public const float Speed = 100.0f;
 	public const float JumpVelocity = -400.0f;
 
 	public Snowball Snowball;
 	public AnimationPlayer AnimationPlayer;
 	public Sprite2D Sprite2D;
 
-	private string mode = "walk";
+	
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 	
 	
-
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -38,41 +38,17 @@ public partial class Villager : CharacterBody2D
 	public override void _Process(double delta)
 	{
 		UpdateLineOfSight();
-		AnimationPlayer.Play(mode);
+		AnimationPlayer.Play("walk");
 		Vector2 velocity = Velocity;
 
 		// Add the gravity.
 		if (!IsOnFloor())
 			velocity.Y += gravity * (float)delta;
+		
+		Position += new Vector2(Speed * (float)delta,0);
+		
+			
 
-		mode = Position.DistanceTo(Snowball.Position) < MinDistance ? "walk" : "idle";
-
-		//if mode is walk, walk towards snowball. Take care to flip the sprite if the snowball is to the left.
-		if (mode == "walk" && NotOnEdge())
-		{
-			if (Position.X < Snowball.Position.X)
-			{
-				velocity.X = Speed;
-				Sprite2D.FlipH = false;
-			}
-			else
-			{
-				velocity.X = -Speed;
-				Sprite2D.FlipH = true;
-			}
-		}
-
-		if (Position.DistanceTo(Snowball.Position) < MinDistance)
-		{
-			if (Position.X < Snowball.Position.X)
-			{
-				Sprite2D.FlipH = false;
-			}
-			else
-			{
-				Sprite2D.FlipH = true;
-			}
-		}
 
 		Velocity = velocity;
 		MoveAndSlide();
@@ -84,7 +60,6 @@ public partial class Villager : CharacterBody2D
 			{
 				//Damage snowball and destroy self
 				Snowball.Damage(1);
-				QueueFree();
 				if (LineOfSight != null)
 					LineOfSight.QueueFree();
 			}
@@ -112,30 +87,5 @@ public partial class Villager : CharacterBody2D
 			LineOfSight.DefaultColor = new Color(1, 0, 0);
 		}
 
-	}
-
-	private bool NotOnEdge() 
-	{
-		// Use raycast to check if there is a floor in front of the enemy
-		var spaceState = GetWorld2D().DirectSpaceState;
-		var direction = Sprite2D.FlipH ? -Speed : Speed;
-		var query = PhysicsRayQueryParameters2D.Create(GlobalPosition, GlobalPosition + new Vector2(direction, Speed));
-		var result = spaceState.IntersectRay(query);
-		Variant value;
-		if (result.TryGetValue("normal",out value))
-		{
-			if ((Vector2)value == Vector2.Up || (Vector2)value == Vector2.Down)
-			{
-				return true;
-			}
-			else
-			{
-				return false;
-			}
-		}
-		else
-		{
-			return false;
-		}
 	}
 }

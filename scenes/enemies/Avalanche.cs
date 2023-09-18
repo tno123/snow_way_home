5593aa837ffa3175675eb7cf305d3e260945b118
Tor-Arne <tornord94@gmail.com>
2023-09-18 20:14:05 +0200

    added collapse animation to avalanche

--- scenes/enemies/Avalanche.cs
+++ scenes/enemies/Avalanche.cs
@@ -6,9 +6,12 @@ public partial class Avalanche : Node2D
 	[Export]
 	public float Speed = 200.0f;
 
+	public AnimationPlayer AnimationPlayer;
+
 	private Node2D Endpoint;
 	private Vector2 startPosition;
 	private Vector2 EndPosition;
+	private bool atEnd = false;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -20,15 +23,20 @@ public partial class Avalanche : Node2D
 			GD.Print("Avalanche.cs: Need Endpoint child node: Error: " + e.Message);
 		}
 		EndPosition = Endpoint.GlobalPosition;
+		AnimationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		Position += (new Vector2(-1,0).Rotated(Rotation)) * Speed * (float)delta;
-		if (Position.X < EndPosition.X)
+		if (!atEnd)
 		{
-			Position = startPosition;
+			Position += (new Vector2(-1,0).Rotated(Rotation)) * Speed * (float)delta;
+			if (Position.X < EndPosition.X)
+			{
+				atEnd = true;
+				AnimationPlayer.Play("collapse");
+			}
 		}
 	}
 
@@ -40,5 +48,14 @@ public partial class Avalanche : Node2D
 		}
 
 	}
-}
 
+	private void _on_animation_player_animation_finished(StringName anim_name)
+	{
+		if (anim_name == "collapse")
+		{
+			Position = startPosition;
+			atEnd = false;
+			AnimationPlayer.Play("RESET");
+		}
+	}
+}

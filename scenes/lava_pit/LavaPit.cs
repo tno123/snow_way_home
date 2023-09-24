37aad018fcf92c86adcd03169cefdbd307ea9b45
Tor-Arne <tornord94@gmail.com>
2023-09-24 12:07:47 +0200

    fix

--- scenes/lava_pit/LavaPit.cs
+++ scenes/lava_pit/LavaPit.cs
@@ -3,18 +3,18 @@ using System;
 
 public partial class LavaPit : Area2D
 {
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready() { }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready() { }
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta) { }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta) { }
 
-    private void _on_body_entered(Node2D body)
-    {
-        if (body is Snowball)
-        {
-            var snowball = (Snowball)body;
-            snowball.Death();
-        }
-    }
+	private void _on_body_entered(Node2D body)
+	{
+		if (body is Snowball)
+		{
+			var snowball = (Snowball)body;
+			snowball.Death();
+		}
+	}
 }
--- scenes/snowbank/Snowbank.cs
+++ scenes/snowbank/Snowbank.cs
@@ -3,29 +3,29 @@ using System;
 
 public partial class Snowbank : Node2D
 {
-    public Snowball Snowball;
+	public Snowball Snowball;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready() { }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready() { }
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta) { }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta) { }
 
-    private void _on_area_2d_body_entered(Node2D body)
-    {
-        if (body is Snowball)
-        {
-            Snowball = (Snowball)body;
-            Snowball.Snowbank = this;
-        }
-    }
+	private void _on_area_2d_body_entered(Node2D body)
+	{
+		if (body is Snowball)
+		{
+			Snowball = (Snowball)body;
+			Snowball.Snowbank = this;
+		}
+	}
 
-    private void _on_area_2d_body_exited(Node2D body)
-    {
-        if (body is Snowball)
-        {
-            Snowball.Snowbank = null;
-            Snowball = null;
-        }
-    }
+	private void _on_area_2d_body_exited(Node2D body)
+	{
+		if (body is Snowball)
+		{
+			Snowball.Snowbank = null;
+			Snowball = null;
+		}
+	}
 }

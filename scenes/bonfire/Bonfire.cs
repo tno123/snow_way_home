33bc208b978f8206ca502bc908c849f9acccd0df
Tor-Arne <tornord94@gmail.com>
2023-09-24 12:05:39 +0200

    formatted a bunch of stuff

--- scenes/bonfire/Bonfire.cs
+++ scenes/bonfire/Bonfire.cs
@@ -3,33 +3,33 @@ using System;
 
 public partial class Bonfire : Node2D
 {
-	private AnimatedSprite2D animation;
-	
-	
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		animation = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-	}
+    private AnimatedSprite2D animation;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		animation.Play("default");
-	}
-	protected virtual void _on_area_2d_body_entered(Node2D body)
-	{
-		if (body is Snowball){
-			var snowball = (Snowball)body;
-			if(snowball.GetNode<Timer>("DamageBoostTimer").IsStopped()){
-				snowball.Damage(1);
-				snowball.Scale = new Vector2((float)(((Snowball)body).Scale.X/1.5), (float)(((Snowball)body).Scale.Y/1.5));
-			}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        animation = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+    }
 
-		}
-	}
-	
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        animation.Play("default");
+    }
+
+    protected virtual void _on_area_2d_body_entered(Node2D body)
+    {
+        if (body is Snowball)
+        {
+            var snowball = (Snowball)body;
+            if (snowball.GetNode<Timer>("DamageBoostTimer").IsStopped())
+            {
+                snowball.Damage(1);
+                snowball.Scale = new Vector2(
+                    (float)(((Snowball)body).Scale.X / 1.5),
+                    (float)(((Snowball)body).Scale.Y / 1.5)
+                );
+            }
+        }
+    }
 }
-
-
-
--- scenes/puddle/Puddle.cs
+++ scenes/puddle/Puddle.cs
@@ -3,22 +3,20 @@ using System;
 
 public partial class Puddle : Node2D
 {
-	[Signal]
-	public delegate void PuddleEnteredEventHandler(bool isIce);
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    [Signal]
+    public delegate void PuddleEnteredEventHandler(bool isIce);
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
-	private void _on_area_2d_body_entered(Node2D body)
-	{
-		if (body is Snowball)
-		{
-			EmitSignal(SignalName.PuddleEntered,true);
-		}
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready() { }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta) { }
+
+    private void _on_area_2d_body_entered(Node2D body)
+    {
+        if (body is Snowball)
+        {
+            EmitSignal(SignalName.PuddleEntered, true);
+        }
+    }
 }

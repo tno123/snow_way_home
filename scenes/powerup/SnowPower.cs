f1d6d5a28e2c6a8cbd5acec07154cbe6a555e83d
Tor-Arne <tornord94@gmail.com>
2023-09-30 18:20:03 +0200

    formatted stuff from tabs to spaces

--- scenes/powerup/SnowPower.cs
+++ scenes/powerup/SnowPower.cs
@@ -3,24 +3,24 @@ using System;
 
 public partial class SnowPower : ProgressBar
 {
-	//Temp child
-	public Sprite2D Sprite;
+    //Temp child
+    public Sprite2D Sprite;
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		//Assume child of CanvasLayer
-		var snowball = GetParent().GetParent().GetNode<Snowball>("Snowball");
-		MaxValue = snowball.Power;
-		Value = snowball.Power;
-		snowball.Powerup += PowerupCollected;
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        //Assume child of CanvasLayer
+        var snowball = GetParent().GetParent().GetNode<Snowball>("Snowball");
+        MaxValue = snowball.Power;
+        Value = snowball.Power;
+        snowball.Powerup += PowerupCollected;
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta) { }
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta) { }
 
-	private void PowerupCollected(int value)
-	{
-		Value += value;
-	}
+    private void PowerupCollected(int value)
+    {
+        Value += value;
+    }
 }

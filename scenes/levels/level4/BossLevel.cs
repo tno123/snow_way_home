bf8dbfec253f8efc8d65dd54dffc29828978a10d
Tor-Arne <tornord94@gmail.com>
2023-09-30 04:29:30 +0200

    started on boss level

---
+++ scenes/levels/level4/BossLevel.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public partial class BossLevel : Node
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		var ground = GetNode<StaticBody2D>("Ground");
+		var groundChildren = ground.GetChildren();
+
+		for (int i = 0; i < groundChildren.Count; i++)
+		{
+			if (groundChildren[i] is CollisionPolygon2D)
+			{
+				var colPolygon = (CollisionPolygon2D)groundChildren[i];
+				var polygon = colPolygon.GetNode<Polygon2D>("Polygon2D");
+				polygon.Polygon = colPolygon.Polygon;
+			}
+		}
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta) { }
+}

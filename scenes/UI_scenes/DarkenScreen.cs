a200794c9c37c8cb704526c10ad396e851da43d7
Tor-Arne <tornord94@gmail.com>
2023-10-01 08:46:10 +0200

    working on Boss

--- scenes/UI_scenes/DarkenScreen.cs
+++ scenes/UI_scenes/DarkenScreen.cs
@@ -3,54 +3,54 @@ using System;
 
 public partial class DarkenScreen : Control
 {
-    public ColorRect DarkenRect;
-    private bool Darken = false;
-    private Tween tween;
-    private Tween resetTween;
-    private Snowball snowball;
+	public ColorRect DarkenRect;
+	private bool Darken = false;
+	private Tween tween;
+	private Tween resetTween;
+	private Snowball snowball;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        snowball = GetParent().GetParent().GetNode<Snowball>("Snowball");
-        snowball.DarkenScreen += OnDarkenScreen;
-        DarkenRect = GetNode<ColorRect>("ColorRect");
-        tween = CreateTween();
-        tween.TweenProperty(
-            DarkenRect,
-            "color",
-            new Color(0.3f, 0.3f, 0.3f, 1.0f),
-            snowball.DarkenTime
-        );
-        tween.Stop();
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		snowball = GetParent().GetParent().GetNode<Snowball>("Snowball");
+		snowball.DarkenScreen += OnDarkenScreen;
+		DarkenRect = GetNode<ColorRect>("ColorRect");
+		tween = CreateTween();
+		tween.TweenProperty(
+			DarkenRect,
+			"color",
+			new Color(0.3f, 0.3f, 0.3f, 1.0f),
+			snowball.DarkenTime
+		);
+		tween.Stop();
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-        if (Darken && !tween.IsRunning())
-        {
-            if (!tween.IsValid() || tween == null)
-            {
-                tween = CreateTween();
-                tween.TweenProperty(
-                    DarkenRect,
-                    "color",
-                    new Color(0.3f, 0.3f, 0.3f, 1.0f),
-                    snowball.DarkenTime
-                );
-            }
-            tween.Play();
-        }
-        else if (!Darken && tween.IsRunning())
-        {
-            tween.Stop();
-            DarkenRect.Color = new Color(0.3f, 0.3f, 0.3f, 0.0f);
-        }
-    }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if (Darken && !tween.IsRunning())
+		{
+			if (!tween.IsValid() || tween == null)
+			{
+				tween = CreateTween();
+				tween.TweenProperty(
+					DarkenRect,
+					"color",
+					new Color(0.3f, 0.3f, 0.3f, 1.0f),
+					snowball.DarkenTime
+				);
+			}
+			tween.Play();
+		}
+		else if (!Darken && tween.IsRunning())
+		{
+			tween.Stop();
+			DarkenRect.Color = new Color(0.3f, 0.3f, 0.3f, 0.0f);
+		}
+	}
 
-    void OnDarkenScreen(bool darken)
-    {
-        Darken = darken;
-    }
+	void OnDarkenScreen(bool darken)
+	{
+		Darken = darken;
+	}
 }

f5adf935d545ecb5e19c0ac849f1a21469be3c26
FrederikHR <frederik.rosenvinge@gmail.com>
2023-10-01 18:16:33 +0200

    bonfire blinking working better

--- scenes/bonfire/BlinkingBonfire.cs
+++ scenes/bonfire/BlinkingBonfire.cs
@@ -9,13 +9,20 @@ public partial class BlinkingBonfire : Bonfire
 	[Export]
 	bool VisibleStart;
 
+	[Export]
+	private bool Hot = true;
+
+	private AnimatedSprite2D animation;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		base._Ready(); // This will call the _Ready() from the parent class Bonfire
-		Visible = VisibleStart;
+		//Visible = VisibleStart;
 		blinkTimer = GetNode<Timer>("BlinkTimer");
 		blinkTimer.Start();
+
+		animation = GetNode<AnimatedSprite2D>("BlinkingBonfireAnim");
 
 		collision = GetNode<Area2D>("Area2D").GetNode<CollisionShape2D>("CollisionShape2D");
 	}
@@ -25,28 +32,51 @@ public partial class BlinkingBonfire : Bonfire
 	{
 		base._Process(delta); // This calls the _Process() of the Bonfire, ensuring the animation plays
 
+		if (Hot)
+		{
+			stopAnimation();
+			base.playAnimation();
+			//base._Process(delta); // This calls the _Process() of the Bonfire, ensuring the animation plays
+		}
+		else
+		{
+			base.stopAnimation();
+			playAnimation();
+		}
 		// Your additional code for BlinkingBonfire goes here
 	}
 
 	private void _on_timer_timeout()
 	{
-		if (Visible == true)
+		if (Hot)
 		{
-			Visible = false;
+			Hot = false;
 			collision.Disabled = true;
 		}
 		else
 		{
-			Visible = true;
+			Hot = true;
 			collision.Disabled = false;
 		}
 	}
 
 	protected override void _on_area_2d_body_entered(Node2D body)
 	{
-		if (Visible)
+		if (Hot)
 		{
 			base._on_area_2d_body_entered(body);
 		}
+	}
+
+	protected override void playAnimation()
+	{
+		animation.Visible = true;
+		animation.Play("default");
+	}
+
+	protected override void stopAnimation()
+	{
+		animation.Visible = false;
+		animation.Stop();
 	}
 }
--- scenes/bonfire/Bonfire.cs
+++ scenes/bonfire/Bonfire.cs
@@ -5,7 +5,7 @@ public partial class Bonfire : Node2D
 {
 	private AnimatedSprite2D animation;
 	protected Snowball Snowball = null;
-	
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -15,22 +15,36 @@ public partial class Bonfire : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		animation.Play("default");
+		playAnimation();
 	}
+
 	protected virtual void _on_area_2d_body_entered(Node2D body)
 	{
-		if (body is Snowball){
+		if (body is Snowball)
+		{
 			Snowball = body as Snowball;
 			damageSnowball(Snowball);
-
 		}
 	}
-	protected virtual void damageSnowball(Snowball snowball){
+
+	protected virtual void damageSnowball(Snowball snowball)
+	{
 		snowball.Damage(1);
-		snowball.Scale = new Vector2((float)(((Snowball)snowball).Scale.X/1.5), (float)(((Snowball)snowball).Scale.Y/1.5));
-	}	
-	
+		snowball.Scale = new Vector2(
+			(float)(((Snowball)snowball).Scale.X / 1.5),
+			(float)(((Snowball)snowball).Scale.Y / 1.5)
+		);
+	}
+
+	protected virtual void playAnimation()
+	{
+		animation.Visible = true;
+		animation.Play("default");
+	}
+
+	protected virtual void stopAnimation()
+	{
+		animation.Visible = false;
+		animation.Stop();
+	}
 }
-
-
-

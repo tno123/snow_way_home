bf85c5d014cb9442c14783d4b812802aaf394498
Tor-Arne <tornord94@gmail.com>
2023-10-03 19:47:07 +0200

    changed type

--- scenes/boss/BossHand.cs
+++ scenes/boss/BossHand.cs
@@ -3,18 +3,18 @@ using System;
 
 public partial class BossHand : Node2D
 {
-	[Export]
-	public bool flipped = false;
+    [Export]
+    public bool flipped = false;
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		if (flipped)
-		{
-			GetNode<Sprite2D>("Sprite2D").FlipH = true;
-		}
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        if (flipped)
+        {
+            GetNode<AnimatedSprite2D>("AnimatedSprite2D").FlipH = true;
+        }
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta) { }
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta) { }
 }

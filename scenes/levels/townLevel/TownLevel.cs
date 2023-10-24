c30118b15a0586e13257db880b49e94398699e37
FrederikHR <frederik.rosenvinge@gmail.com>
2023-10-24 20:30:13 +0200

    can't access boostInfo inside UI (code works, not path)

--- scenes/levels/townLevel/TownLevel.cs
+++ scenes/levels/townLevel/TownLevel.cs
@@ -3,19 +3,38 @@ using System;
 
 public partial class TownLevel : Node
 {
-	
 	// Called when the node enters the scene tree for the first time.
 	AnimatedSprite2D boostInfo;
+	ProgressBar SnowPower;
+
+	//bool playing = false;
+
 	public override void _Ready()
 	{
-		boostInfo = GetNode<AnimatedSprite2D>("boostInfo");
-	
+		boostInfo = GetNode<CanvasLayer>("UI").GetNode<AnimatedSprite2D>("boostInfo");
+		SnowPower = GetNode<CanvasLayer>("UI").GetNode<ProgressBar>("SnowPower");
+		boostInfo.Hide();
+		SnowPower.Hide();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		boostInfo.Play("default");
+		if (Input.IsActionPressed("ui_down"))
+		{
+			GD.Print("pressed");
+			boostInfo.Show();
+			SnowPower.Show();
+			boostInfo.Play("default");
+			/*  if (!playing)
+			 {
+				 boostInfo.Play("default");
+				 playing = true;
+			 } */
+		}
+
+		//boostInfo.Hide();
+		//boostInfo.Stop();
 		/*
 		if ((snowball as Snowball).IsBoosting)
 			{
@@ -29,6 +48,4 @@ public partial class TownLevel : Node
 			}
 			*/
 	}
-	
-	
 }

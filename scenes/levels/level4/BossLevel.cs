c0477923c002feec7330851da80b339a0cf10e96
Tor-Arne <tornord94@gmail.com>
2023-11-01 03:53:44 +0100

    added falling death to boss level

--- scenes/levels/level4/BossLevel.cs
+++ scenes/levels/level4/BossLevel.cs
@@ -3,11 +3,15 @@ using System;
 
 public partial class BossLevel : Node
 {
+	private Camera2D camera;
+	private Snowball snowball;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		var ground = GetNode<StaticBody2D>("Ground");
 		var groundChildren = ground.GetChildren();
+		camera = GetNode<Camera2D>("Camera2D");
+		snowball = GetNode<Snowball>("Snowball");
 
 		for (int i = 0; i < groundChildren.Count; i++)
 		{
@@ -21,5 +25,10 @@ public partial class BossLevel : Node
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta) { }
+	public override void _Process(double delta) {
+		if (snowball.GlobalPosition.Y > camera.LimitBottom)
+		{
+			snowball.Death();
+		}
+	 }
 }
